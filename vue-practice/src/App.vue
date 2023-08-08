100b63bcce2360610df434c52efafcebf04dd5ce
AlexKimRav <ravenq1113@gmail.com>
2023-08-08 18:29:44 +0900

    first commit

---
+++ vue-practice/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ product }}</h1>
+        <h2>{{ description }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                product: 'book',
+                description: 'Just some book'
+            }
+        }
+    }
+</script>
+
+<style >
+
+</style>
